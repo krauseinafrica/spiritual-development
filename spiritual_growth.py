8afe541f813504d57a6ed129d3803d001116a674
krauseinafrica <krauseinafrica@icloud.com>
2024-10-04 15:55:50 -0400

    API Key

--- spiritual_growth.py
+++ spiritual_growth.py
@@ -4,7 +4,8 @@ import plotly.graph_objects as go
 import openai
 from openai import OpenAI
 
-client = OpenAI(api_key = "<API_KEY>")
+# client = OpenAI(api_key = "<API_KEY>")
+client = st.secrets["openai"]["API_KEY"]
 
 # Set your OpenAI API key
 # openai.api_key = st.secrets["openai"]["api_key"]
